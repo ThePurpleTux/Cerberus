1b7ca4752839bbad788597f2c2b852ec056ebe74
Joker122402 <dovid.rosoff@gmail.com>
2023-05-19 13:01:39 -0400

    Added mkdir.py

---
+++ cerberus/agent_functions/mkdir.py
@@ -0,0 +1,53 @@
+from mythic_container.MythicCommandBase import *
+import json
+
+class MkdirArguments(TaskArguments):
+
+    def __init__(self, command_line, **kwargs):
+        super().__init__(command_line, **kwargs)
+        self.args = [
+            CommandParameter(
+                name="path",
+                type=ParameterType.String,
+                default_value="",
+                description="Directory to create.",
+                parameter_group_info=[ParameterGroupInfo(
+                    required=False
+                )]
+            )
+        ]
+
+    async def parse_arguments(self):
+        # if len(self.command_line) == 0:
+        #     raise Exception("Requires path to directory you want to move to. \nUsage: {}".format(CdCommand.help_cmd))
+        if self.command_line[0] == "{":
+            self.load_args_from_json_string(self.command_line)
+        else:
+            if self.command_line[0] == '"' and self.command_line[-1] == '"':
+                self.command_line = self.command_line[1:-1]
+            elif self.command_line[0] == "'" and self.command_line[-1] == "'":
+                self.command_line = self.command_line[1:-1]    
+            self.add_arg("path", self.command_line)
+
+class MkdirCommand(CommandBase):
+    cmd = "mkdir"
+    needs_admin = False
+    help_cmd = "mkdir <path>"
+    description = "Create <path>"
+    version = 1
+    author = "@ThePurpleTux"
+    argument_class = MkdirArguments
+    attackmapping = ["T1106"]
+
+
+    async def create_go_tasking(self, taskData: PTTaskMessageAllData) -> PTTaskCreateTaskingMessageResponse:
+        response = PTTaskCreateTaskingMessageResponse(
+            TaskID=taskData.Task.ID,
+            Success=True,
+        )
+        response.DisplayParams = taskData.args.get_arg("path")
+        return response
+    
+    async def process_response(self, task: PTTaskMessageAllData, response: any) -> PTTaskProcessResponseMessageResponse:
+        resp = PTTaskProcessResponseMessageResponse(TaskID=task.Task.ID, Success=True)
+        return resp
