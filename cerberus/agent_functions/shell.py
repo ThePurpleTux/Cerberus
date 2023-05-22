504bfa51182d89e4a65596c02beb12662a0b23cf
Joker122402 <dovid.rosoff@gmail.com>
2023-05-22 09:37:17 -0400

    updated run.py. Added shell.py

--- cerberus/agent_functions/shell.py
+++ cerberus/agent_functions/shell.py
@@ -1,59 +1,45 @@
 from mythic_container.MythicCommandBase import *
 import json
 
-class RunArguments(TaskArguments):
+class ShellArguments(TaskArguments):
 
     def __init__(self, command_line, **kwargs):
         super().__init__(command_line, **kwargs)
         self.args = [
             CommandParameter(
-                name="binary",
-                cli_name="Binary",
-                display_name="Binary",
-                type=ParameterType.String,
-                description="Path to a binary to execute.",
-                parameter_group_info=[ParameterGroupInfo(
-                    required=True,
-                    ui_position=0
-                )]
-            ),
-            CommandParameter(
                 name="arguments",
                 cli_name="Arguments",
                 display_name="Arguments",
                 type=ParameterType.String,
-                default_value="",
-                description="[optional] Arguments",
+                description="Arguments to supply to cmd.exe",
                 parameter_group_info=[ParameterGroupInfo(
-                    required=False,
-                    ui_position=1
+                    required=True,
+                    ui_position=0
                 )]
             )
         ]
 
     async def parse_arguments(self):
         if len(self.command_line.strip()) == 0:
-            raise Exception("Requires path to an executable. \nUsage: {}".format(RunCommand.help_cmd))
+            raise Exception("Requires arguments to supply to cmd.exe. \nUsage: {}".format(RunCommand.help_cmd))
 
         if self.command_line[0] == "{":
             self.load_args_from_json_string(self.command_line)
         else:
             parts = self.command_line[0].split(" ", 1)
-            self.add_arg("binary", parts[0])
-            if (len(parts) > 1):
-                self.add_arg("arguments", parts[1])
+            self.add_arg("arguments", parts[0])
 
-class RunCommand(CommandBase):
-    cmd = "run"
+class ShellCommand(CommandBase):
+    cmd = "shell"
     needs_admin = False
-    help_cmd = "run <binary> <arguments>"
-    description = "Execute a binary on the target system."
+    help_cmd = "shell <arguments>"
+    description = "Use cmd.exe to execute a command on the target system."
     version = 1
     author = "@ThePurpleTux"
-    argument_class = RunArguments
+    argument_class = ShellArguments
     attackmapping = ["T1106", "T1218", "T1553"]
     attributes = CommandAttributes(
-        suggested_command=True
+        suggested_command=False
     )
 
 
@@ -62,8 +48,8 @@ class RunCommand(CommandBase):
             TaskID=taskData.Task.ID,
             Success=True,
         )
-        response.DisplayParams = "-Binary {} -Arguments {}".format(
-            taskData.args.get_arg("binary"), taskData.args.get_arg("arguments")
+        response.DisplayParams = "-Arguments {}".format(
+            taskData.args.get_arg("arguments")
         )
         return response
     
