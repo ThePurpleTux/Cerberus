73d353d064c4aebcb7dd8448a7a5323de00d4a4d
Joker122402 <dovid.rosoff@gmail.com>
2023-05-31 07:06:10 -0700

    update config

--- cerberus/agent_code/Cerberus/Config.cs
+++ cerberus/agent_code/Cerberus/Config.cs
@@ -11,7 +11,7 @@ namespace Cerberus
         public static string ServerAddress { get; set; } = "10.0.2.128";
         public static int ServerPort { get; set; } = 80;
         public static int SleepTime { get; set; } = 5000;
-        public static string PayloadUUID { get; set; } = "d13366ca-1cff-4b88-9110-e2c1a85f216s";
+        public static string PayloadUUID { get; set; } = "d13366ca-1cff-4b88-9110-e2c1a85f216b";
         public static string killdateString { get; set; } = "2023-06-24";
         public static string URI { get; set; } = "/data";
         public static string UserAgent { get; set; } = "Joker";
